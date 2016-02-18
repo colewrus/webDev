7928b4b47f8086dc422588ffd7a73c7245a808c4
colewrus <wrampelc@gmail.com>
2016-02-18 17:53:39 -0500

    partial homework complete for week 5

---
+++ week5/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+   $('li').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+   $('li').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+       }); 
+   });
+   
+});
