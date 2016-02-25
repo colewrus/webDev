306765f92663fc171cf93feb4b2b4822240f4a7f
colewrus <wrampelc@gmail.com>
2016-02-25 15:41:22 -0500

    done before class

--- final5/script.js
+++ final5/script.js
@@ -5,22 +5,25 @@ $(document).ready(function() {
 		$('#resume-pdf').removeClass("visible");
 		$('#personal-area').addClass("visible");
 		$('#personal').css('z-index', '1');
+		$('#resume-pdf').css('pointer-events','auto');
    });  
 
 	$('#personal').click(function(){
 		$('#resume-pdf').addClass("visible");
 		$('#personal-area').removeClass("visible");
+		$('#resume-pdf').css('pointer-events','none');
 		$('#personal').css('z-index', '3');
    });   
    
    
 	$('#resume-pdf').addClass("visible");
+	$('#resume-pdf').css('pointer-events','none');
 	
    $(function(){
 		var x = $('#resume-pdf').parent().width();
-		var y = $('#resume-pdf').parent().height();
+		var navX = $('.col-md-8').width();
 		$('#resume-pdf').width(x);
-		$('#resume-pdf').height(y);
+		$('.navbar').width(navX);
    })
 	
 	$('.col-md-1').click(function(){
@@ -37,5 +40,7 @@ $(window).resize(function() {
 	$('#resume-pdf').parent().height(y);
     
 });
+
+
 
 //ask for array append code
