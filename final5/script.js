c5c6ed77ec61f610adb59f0126c238542acf5cf8
colewrus <wrampelc@gmail.com>
2016-02-25 13:57:05 -0500

    tab images in and working. Needs: personal content, background formatting, work on header

--- final5/script.js
+++ final5/script.js
@@ -2,15 +2,40 @@ $(document).ready(function() {
 	
    
    $('#resume').click(function(){
-		$('#resume-pdf').toggleClass("visible");
-   });   
+		$('#resume-pdf').removeClass("visible");
+		$('#personal-area').addClass("visible");
+		$('#personal').css('z-index', '1');
+   });  
 
+	$('#personal').click(function(){
+		$('#resume-pdf').addClass("visible");
+		$('#personal-area').removeClass("visible");
+		$('#personal').css('z-index', '3');
+   });   
+   
+   
+	$('#resume-pdf').addClass("visible");
+	
    $(function(){
 		var x = $('#resume-pdf').parent().width();
+		var y = $('#resume-pdf').parent().height();
 		$('#resume-pdf').width(x);
-		$('#resume-pdf').addClass("visible");
+		$('#resume-pdf').height(y);
    })
-  
+	
+	$('.col-md-1').click(function(){
+		console.log($(this).width());
+	})
+	
+});
+
+
+$(window).resize(function() {
+	var x = $('#resume-pdf').parent().width();
+	var y = $('#resume-pdf').height();
+	$('#resume-pdf').width(x);
+	$('#resume-pdf').parent().height(y);
+    
 });
 
 //ask for array append code
